d785f90b9b325899bba0306e3fc6ad134258132a
g1jayara <g.jayaraj@nokia.com>
2025-06-24 00:00:00 +0530

    Gallery item inner buttons addition

--- src/components/galleryItem/GalleryItem.jsx
+++ src/components/galleryItem/GalleryItem.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./GalleryItem.css"
+import share from "/general/share.svg";
+import more from "/general/more.svg";
 
 const GalleryItem = ({item}) => {
   return (
     <div className='gallery-item' style={{gridRowEnd: `span ${Math.ceil(item.height/100)}`}}>
-        <img src={item.media} alt="Image Post" />
+        <img className='main-img' src={item.media} alt="Image Post" />
+        <button className='button save'>Save</button>
+        {/* <div style={{display: 'flex', gap: '10px'}}> */}
+          <button className='button share'><img src={share} alt="" /></button>
+          <button className='button more-actions'><img src={more} alt="" /></button>
+        {/* </div> */}
     </div>
   )
 }
